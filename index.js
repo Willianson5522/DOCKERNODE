b159e73d9d8335ac1fea84d5a2e9b3e6db6b92b3
Willianson Silva <williansoncesar@gmail.com>
2022-08-28 16:12:02 -0300

    adicionado conexao mongodb e modulo dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // config inicial
 
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express();
+require('dotenv').config();
 
 // forma de ler JSON - Middlewares
 
@@ -21,6 +23,13 @@ app.get('/', (req, res) => {
     res.json({ message: 'Oi Express!' })
 });
 
-// entregar uma porta
+// conexao com banco de dados & entrega de porta
 
-app.listen(PORT, HOST);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusternodeapi.zwmpdhk.mongodb.net/DBNodeAPI?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Conectado ao MongoDB!');
+        app.listen(PORT, HOST);
+    })
+    .catch((err) => {
+        console.log(err)
+    });
