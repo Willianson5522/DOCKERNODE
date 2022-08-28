4702e240014b16a605d0ab89fef383c3654f3986
Willianson Silva <williansoncesar@gmail.com>
2022-08-28 18:20:19 -0300

    adicionado rota para leitura de dados

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -37,4 +37,16 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Read - leitura de dados
+
+router.get('/', async (req, res) => {
+    try {
+        const people = await Person.find()
+        res.status(200).json(people)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+})
+
 module.exports = router;
