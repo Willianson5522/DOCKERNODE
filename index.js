af085b159ee5d30fc3ee887195d9a8fc3245f841
Willianson Silva <williansoncesar@gmail.com>
2022-08-28 17:02:08 -0300

    adicionado rota post criar dados

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // config inicial
-
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const PORT = 3000;
@@ -9,7 +8,6 @@ require('dotenv').config();
 const Person = require('./models/Person');
 
 // forma de ler JSON - Middlewares
-
 app.use(
     express.urlencoded({
         extended: true,
@@ -18,14 +16,37 @@ app.use(
 
 app.use(express.json());
 
-// rota inicial / endpoint
+// rotas da API
+app.post('/Person', async (req, res) => {
 
+    //req.body
+
+
+    //destructuring assignment
+    //{name: "willianson", salary: 9999, approved: false}
+    const { name, salary, approved } = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    try {
+        //criando dados
+        await Person.create(person);
+        res.status(201).json({message: 'Pessoa inserida no sistema com sucesso!'})
+    } catch (error) {
+        req.status(500).json({ error: error })
+    }
+})
+
+// rota inicial / endpoint
 app.get('/', (req, res) => {
     res.json({ message: 'Oi Express!' })
 });
 
 // conexao com banco de dados & entrega de porta
-
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusternodeapi.zwmpdhk.mongodb.net/DBNodeAPI?retryWrites=true&w=majority`)
     .then(() => {
         console.log('Conectado ao MongoDB!');
