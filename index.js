6c6142557ec0fb67410909672e219a7921216de1
Willianson Silva <williansoncesar@gmail.com>
2022-08-28 13:59:53 -0300

    adicionado API leitura/envio json

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express');
+// config inicial
 
+const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
-
 const app = express();
 
+// forma de ler JSON - Middlewares
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json());
+
+// rota inicial / endpoint
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.json({ message: 'Oi Express!' })
 });
+
+// entregar uma porta
 
 app.listen(PORT, HOST);
